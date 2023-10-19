337087d5a85daa4bcb4e4ae7f64eb63097563381
Shivangi Mall <mallshivangi03@gmail.com>
2023-10-19 13:23:59 +0530

    eposide1

---
+++ App.js
@@ -0,0 +1,6 @@
+
+
+
+const heading = React.createElement("div", {id:"child"},[React.createElement("h1", {},"Hello from react"),React.createElement("h1", {},"Hello from react")]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
